fd1dbe6c6676a28fca678b35cc91e2cbabb21b46
임근찬 <imgeunchan@MI-1-chanlim.local>
2024-03-25 17:58:31 +0900

    triton python be kwargs specified

--- model-serving/triton/models/python_backend/1/model.py
+++ model-serving/triton/models/python_backend/1/model.py
@@ -4,33 +4,51 @@ from transformers import AutoModelForSeq2SeqLM
 import numpy as np
 import torch
 import triton_python_backend_utils as pb_utils
+import json
 
 
-def build_input(requests: list):
+def build_input(requests: list, device: str):
     batch_sizes = [np.shape(pb_utils.get_input_tensor_by_name(request, "INPUT_IDS").as_numpy()) for request in requests]
     max_len = np.max([bs[1] for bs in batch_sizes])
-    input_ids = torch.tensor(np.concatenate([np.pad(
+
+    each_ids = [
+        np.pad(
             pb_utils.get_input_tensor_by_name(request, "INPUT_IDS").as_numpy(),
             ((0, 0), (0, max_len - batch_size[1])),
-            ) for batch_size, request in zip(batch_sizes, requests)], axis=0,)
-    ).to("cuda")
+        ) for batch_size, request in zip(batch_sizes, requests)
+    ]
+    input_ids = torch.tensor(np.concatenate(each_ids, axis=0)).to(device)
+
     attention_mask = torch.tensor(
         (
             np.arange(max_len).repeat(len(requests)).reshape(max_len, len(requests))
             < [bs[1] for bs in batch_sizes]
         ).T
-    ).to("cuda")
+    ).to(device)
+
     return batch_sizes, input_ids, attention_mask
 
 
 class TritonPythonModel:
-    def initialize(self, args):
-        self.model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M").to("cuda")
+    def initialize(self, kwargs):
+        self.model_config: dict = json.loads(kwargs["model_config"]) # json string of config.pbtxt (auto-configured or not)
+        self.model_instance_kind: str = kwargs["model_instance_kind"] # CPU | GPU
+        self.model_instance_name: str = kwargs["model_instance_name"] # <model_name>_<group_id>_<instance_id>
+        self.model_instance_device_id: int = int(kwargs["model_instance_device_id"]) # 0
+        self.model_repository: str = kwargs["model_repository"] # /data/models/<model_name>/
+        self.model_version: int = kwargs["model_version"] # 1
+        self.model_name: str = kwargs["model_name"] # <model_name>
+        pb_utils.Logger.log_info(f"model_config <value={self.model_config}>")
+        pb_utils.Logger.log_info(f"pb_utils <attrs={dir(pb_utils)}>")
+
+        self.model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M", device_map="auto")
+        pb_utils.Logger.log_info(f"model_initialized <device={self.model.device}>")
 
     def execute(self, requests: list):
-        batch_sizes, input_ids, attention_mask = build_input(requests)
+        batch_sizes, input_ids, attention_mask = build_input(requests, self.model.device)
         responses = []
-        translated_tokens = self.model.generate(input_ids=input_ids, 
+        translated_tokens = self.model.generate(
+            input_ids=input_ids, 
             attention_mask=attention_mask,
             forced_bos_token_id=256042 # German Language token
         ).to("cpu")
